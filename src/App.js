f8a844a2c23701db8b73b562da4af3e33b67076c
dedyrahmat <acesonix@gmail.com>
2021-03-12 11:53:24 +0700

    Fixing image size

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {source && <img src={source} alt="logo" />}
+        {source && <img src={source} style={{ width: "100%" }} alt="logo" />}
         <input
           accept="image/*"
           id="icon-button-file"
